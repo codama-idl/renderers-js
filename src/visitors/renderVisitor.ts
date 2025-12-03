7ae30feb7158f88d86e4960ddee53f6d3d8527ff
Loris Leiva <loris.leiva@gmail.com>
2025-12-03 11:22:45 +0100

    Use file path when formatting and format updated package.json

--- src/visitors/renderVisitor.ts
+++ src/visitors/renderVisitor.ts
@@ -1,7 +1,7 @@
-import { deleteDirectory, writeRenderMap } from '@codama/renderers-core';
+import { deleteDirectory, mapRenderMapContentAsync, writeRenderMap } from '@codama/renderers-core';
 import { rootNodeVisitor, visit } from '@codama/visitors-core';
 
-import { formatCode, RenderOptions, syncPackageJson } from '../utils';
+import { getCodeFormatter, RenderOptions, syncPackageJson } from '../utils';
 import { getRenderMapVisitor } from './getRenderMapVisitor';
 
 export function renderVisitor(path: string, options: RenderOptions = {}) {
@@ -15,10 +15,11 @@ export function renderVisitor(path: string, options: RenderOptions = {}) {
         let renderMap = visit(root, getRenderMapVisitor(options));
 
         // Format the code, if requested.
-        renderMap = await formatCode(renderMap, options);
+        const formatCode = await getCodeFormatter(options);
+        renderMap = await mapRenderMapContentAsync(renderMap, formatCode);
 
         // Create or update package.json dependencies, if requested.
-        syncPackageJson(renderMap, options);
+        await syncPackageJson(renderMap, formatCode, options);
 
         // Write the rendered files to the output directory.
         writeRenderMap(renderMap, path);
