6d08d919dc5e0108a2fc4ec5b72c9a752d905e8d
Callum McIntyre <callum.mcintyre@anza.xyz>
2026-01-29 15:16:33 +0000

    Bump to Prettier 3.8.1 (#100)

--- test/e2e/anchor/src/generated/programs/wenTransferGuard.ts
+++ test/e2e/anchor/src/generated/programs/wenTransferGuard.ts
@@ -101,11 +101,10 @@ export function identifyWenTransferGuardInstruction(
 }
 
 export type ParsedWenTransferGuardInstruction<TProgram extends string = 'LockdqYQ9X2kwtWB99ioSbxubAmEi8o9jqYwbXgrrRw'> =
-
-        | ({ instructionType: WenTransferGuardInstruction.CreateGuard } & ParsedCreateGuardInstruction<TProgram>)
-        | ({ instructionType: WenTransferGuardInstruction.Execute } & ParsedExecuteInstruction<TProgram>)
-        | ({ instructionType: WenTransferGuardInstruction.Initialize } & ParsedInitializeInstruction<TProgram>)
-        | ({ instructionType: WenTransferGuardInstruction.UpdateGuard } & ParsedUpdateGuardInstruction<TProgram>);
+    | ({ instructionType: WenTransferGuardInstruction.CreateGuard } & ParsedCreateGuardInstruction<TProgram>)
+    | ({ instructionType: WenTransferGuardInstruction.Execute } & ParsedExecuteInstruction<TProgram>)
+    | ({ instructionType: WenTransferGuardInstruction.Initialize } & ParsedInitializeInstruction<TProgram>)
+    | ({ instructionType: WenTransferGuardInstruction.UpdateGuard } & ParsedUpdateGuardInstruction<TProgram>);
 
 export function parseWenTransferGuardInstruction<TProgram extends string>(
     instruction: Instruction<TProgram> & InstructionWithData<ReadonlyUint8Array>,
